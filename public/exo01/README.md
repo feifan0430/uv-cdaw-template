87493bae594e9898050aa5971d42d899d13a6753
feifan0430 <feifan0430@users.noreply.github.com>
2021-11-08 13:57:37 +0100

    exo01 version1.0

---
+++ public/exo01/README.md
@@ -0,0 +1,8 @@
+# Auteur(s)
+- <FEI Fan>
+
+# Exo 1
+
+<description>
+
+Afficher l'heure courante
--- public/exo01/index.php
+++ public/exo01/index.php
@@ -1,9 +1,9 @@
 <?php
 
 // header("content-type:text/html;charset=utf-8");
+
 date_default_timezone_set("Europe/Paris");
 header("refresh: 1");
 echo 'Heure courante: ' . date("h/i/sa d/m/Y") . '<br>';
-
 
 ?>
