579f57c8467d791f4119e314118b36aced5cc813
feifan0430 <feifan0430@users.noreply.github.com>
2021-11-20 21:39:17 +0100

    Laravel CRUD

--- public/jalon2/catalogue/database/migrations/2021_11_19_141430_create_liste_film_table.php
+++ public/jalon2/catalogue/database/migrations/2021_11_19_141430_create_liste_film_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+// Lavarel CRUD
 class CreateListeFilmTable extends Migration
 {
     /**
