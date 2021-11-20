cf7c8f21c23bfa8db4f4633538c0f7b8d1da0dec
feifan0430 <feifan0430@users.noreply.github.com>
2021-11-20 21:17:59 +0100

    jalon 2

--- public/jalon2/catalogue/resources/views/explore.blade.php
+++ public/jalon2/catalogue/resources/views/explore.blade.php
@@ -1,89 +1,5 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
-    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
-    <meta name="author" content="Fan FEI">
-    <meta name="description" content="UV CDAW Jalon1">
-    <!-- jquery -->
-    <script src='{{ asset("scripts/jquery-3.5.1.min.js") }}'></script>
-    <!-- Bootstrap css-->
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
-    <!-- JavaScript -->
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-    <!-- Explore style -->
-    <link rel="stylesheet" href="{{ asset('css/star-rating.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- Explore JavaScript -->
-    <script src="{{ asset('js/star-rating.js') }}"></script>
-    <title>IDAW Film</title>
-</head>
-<body>
-    <div class="container-fluid" style="margin-bottom: 0%;">
-        <div class="row">
-            <div class="navbar navbar-default navbar-inverse" role="navigation" style="padding-right: 2%;">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="index.html">
-                        cdawFilm
-                    </a>
-                </div>
-                <div class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav">
-                        <li>
-                            <a href="{{ route('index') }}" class="active">Accueil</a>
-                        </li>
-                        <li>
-                            <a href="{{ route('explore') }}">Catagories</a>
-                        </li>
-                        <li>
-                            <a href="{{ route('user') }}">Mon espace</a>
-                        </li>
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li>
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                Mon compte  
-                                <b class="caret"></b>
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li>
-                                    <a href="#">Bonjour</a>
-                                </li>
-                                <li class="divider"></li>
-                                <li>
-                                    <a href="{{ route('user') }}">Your Profile</a>
-                                </li>
-                                <li>
-                                    <a href="{{ route('signin') }}">Sign in</a>
-                                </li>
-                                <li>
-                                    <a href="{{ route('signup') }}">Sign up</a>
-                                </li>
-                                <li class="divider"></li>
-                                <li>
-                                    <a href="#">Log out</a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="container" style="margin-bottom: 2%;">
-        <div class="row">
-            <div class="col-md-4 col-md-push-1" style="height: 40px;">
-                <div class="input-group" style="margin-top: 10px;">
-                    <input type="text" class="form-control"placeholder="Que vous voulez..." / >
-                    <span class="input-group-btn">
-                        <button class="btn btn-info btn-search" style="background-color: gray;">cherche</button>
-                    </span>
-                </div>
-            </div>
-        </div>
-    </div>
-    
+@extends('layouts.cdaw_film')
+@section('content')
     <div class="container">
         <div class="row">
             <span class="heading">
@@ -467,5 +383,4 @@
             </div>
         </div>
     </div>
-</body>
-</html>
+@endsection
