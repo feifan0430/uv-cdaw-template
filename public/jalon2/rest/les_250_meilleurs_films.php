be25e957e7dce080d27a193175ef1e57edcd16e6
feifan0430 <feifan0430@users.noreply.github.com>
2021-11-20 21:25:21 +0100

    les 250 meilleurs films

--- public/jalon2/rest/les_250_meilleurs_films.php
+++ public/jalon2/rest/les_250_meilleurs_films.php
@@ -5,7 +5,7 @@
     <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
     <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
     <meta name="author" content="Fan FEI">
-    <meta name="description" content="IMDB api">
+    <meta name="description" content="IMDB api: les 250 meilleurs films">
     <title>CDAW api demo</title>
 </head>
 <body>
